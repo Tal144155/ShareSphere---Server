1e72e3991819cebe6a0f44d4f66ec29a5703bd4a
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-23 17:36:40 +0200

    editing to work with token only

--- models/token.js
+++ models/token.js
@@ -1,43 +1,41 @@
-const express = require('express')
+const express = require("express");
 
-const jwt = require("jsonwebtoken")
-const key = "keypassphrase"
+const jwt = require("jsonwebtoken");
+const key = "keypassphrase";
 
 const userInfo = (req, res) => {
-    res.json({ user })
-}
+  res.json({ user });
+};
 
 // Generate a token
 function getToken(req) {
-    const data = { username: req.body.username }
-    // Generate the token.
-    const token = jwt.sign(data, key)
-    // Return the token to the browser
-    return token
+  const data = { username: req.body.username };
+  // Generate the token.
+  const token = jwt.sign(data, key);
+  // Return the token to the browser
+  return token;
 }
 
 // Ensure the user sent a valid token
 function isLoggedIn(req, res, next) {
-    // If the request has an authorization header
-    if (req.headers.authorization) {
-        // Extract the token from that header
-        const token = req.headers.authorization.split(" ")[1];
-        try {
-            // Verify the token is valid
-            const data = jwt.verify(token, key);
-            // remove this line
-            console.log('The logged in user is: ' + data.username);
-            // Token validation was successful. Continue to the actual function
-            return next()
-        } catch (err) {
-            return res.status(401).json( { error: "Invalid Token" } );
-        }
+  // If the request has an authorization header
+  if (req.headers.authorization) {
+    // Extract the token from that header
+    const token = req.headers.authorization;
+    try {
+      // Verify the token is valid
+      const data = jwt.verify(token, key);
+      // remove this line
+      console.log("The logged in user is: " + data.username);
+      // Token validation was successful. Continue to the actual function
+      return next();
+    } catch (err) {
+      return res.status(401).json({ error: "Invalid Token" });
     }
-    else
-        return res.status(403).json( { error: 'Token required' } );
+  } else return res.status(403).json({ error: "Token required" });
 }
 
 module.exports = {
-    getToken,
-    isLoggedIn
-}
+  getToken,
+  isLoggedIn,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const userController = require("../controllers/user");
+const tokenModel = require('../models/token.js')
+
 
 const express = require("express");
 
@@ -6,6 +8,6 @@ var router = express.Router();
 
 router.route("/").post(userController.createUser);
 
-router.route("/:id").get(userController.getUser);
+router.route("/:id").get(tokenModel.isLoggedIn, userController.getUser);
 
 module.exports = router;
