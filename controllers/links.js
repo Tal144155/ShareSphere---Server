9067499c17c853afbe23d1e1f7761bfdc85ee531
Tal144155 <tal.ariel.ziv@gmail.com>
2024-04-14 23:22:46 +0300

    editing to work with one cclient socket

--- controllers/links.js
+++ controllers/links.js
@@ -10,44 +10,41 @@ const checkListUrl = async (req, res) => {
 
   let isBadFound = false;
   let completedRequests = 0;
+  const client = new net.Socket();
+
+  client.connect(5555, "192.168.174.129", () => {
+    console.log("Connected to TCP server");
+  });
 
   ListURL.forEach((url) => {
-    const client = net.createConnection(
-      { port: 5555, host: "192.168.174.129" },
-      () => {
-        console.log("Connected to TCP server");
+    client.write(`check-url ${url}`);
+  });
 
-        // Send URL to the TCP server
-        client.write(`check-url ${url}`);
+  client.on("data", (data) => {
+    console.log(`Received data from TCP server: ${data}`);
+
+    if (data.toString() === "true") {
+      isBadFound = true;
+    }
+
+    completedRequests++;
+
+    if (completedRequests === ListURL.length) {
+      if (isBadFound) {
+        res.status(403).json(false);
+      } else {
+        res.status(200).json(true);
       }
-    );
+    }
+  });
 
-    client.on("data", (data) => {
-      console.log(`Received data from TCP server: ${data}`);
+  client.on("end", () => {
+    console.log("Disconnected from TCP server");
+  });
 
-      if (data.toString() === "true") {
-        isBadFound = true;
-      }
-
-      client.end();
-    });
-
-    client.on("end", () => {
-      console.log("Disconnected from TCP server");
-      completedRequests++;
-
-      if (completedRequests === ListURL.length) {
-        if (isBadFound) {
-          return res.status(403).json(false);
-        }
-        return res.status(200).json(true);
-      }
-    });
-
-    client.on("error", (err) => {
-      console.error("Error connecting to TCP server:", err);
-      return res.status(500).json({ error: "Error connecting to TCP server" });
-    });
+  client.on("error", (err) => {
+    console.error("Error connecting to TCP server:", err);
+    res.status(500).json({ error: "Error connecting to TCP server" });
   });
 };
 
